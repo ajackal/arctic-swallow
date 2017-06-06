b7953425bee3a41f26ee60e1a30a8d9fa835ba09
chris miller <ajackal244@gmail.com>
2017-06-06 15:59:03 -0400

    revised responses, enchanced logging

--- arctic-swallow.py
+++ arctic-swallow.py
@@ -1,4 +1,4 @@
-# this is the initial honey pot file
+
 import SocketServer
 import sys
 from threading import Thread
@@ -35,37 +35,63 @@ class SMBHandler(SocketServer.StreamRequestHandler):
         smb_session_startup_response = "pcaps/smb_session_response_win10"
         smb_account_disabled_response = "pcaps/smb_account_disabled_response_win10"
         smb_session_close_response = "pcaps/smb_session_close_response"
-        with open(smb_negotiate_response, 'rb') as f:
-            # Set variable with SMB response
-            smb_response_win10_home = f.read()
         # Get DATA from socket
-        self.DATA = self.request.recv(BUFFER_SIZE).strip()
-        # Convert DATA to hex
-        pkt_hex = ""
-        for i in self.DATA:
-            # Converts each byte to hex
-            pkt_hex_byte = hex(ord(i))
-            # Constructs hex bytes together in one string
-            pkt_hex += pkt_hex_byte
-        # Check DATA for SMB Header in Hex
-        if smb_header_negotiate in pkt_hex:
-            # Send response if Header is found.
-            print "[*] SMB Header detected."
-            self.request.sendall(smb_response_win10_home)
-        if smb_header_session_setup in pkt_hex:
-            # Send account disabled response to start up request.
-            print "[*] SMB Session Setup request detected."
-            self.request.sendall(smb_session_startup_response)
-        if smb_header_account_disabled in pkt_hex:
-            # Send LANMAN info to requester
-            print "[*] SMB Account Disabled response detected."
-            self.request.sendall(smb_account_disabled_response)
-        if smb_session_close in pkt_hex:
-            # Send session close.
-            print "[*] SMB Session close detected."
-            self.request.sendall(smb_session_close_response)
-        else:
-            self.request.sendall(self.DATA)
+        try:
+            self.DATA = self.request.recv(BUFFER_SIZE).strip()
+            # Convert DATA to hex
+            pkt_hex = ""
+            for i in self.DATA:
+                # Converts each byte to hex
+                pkt_hex_byte = hex(ord(i))
+                # Constructs hex bytes together in one string
+                pkt_hex += pkt_hex_byte
+            # Check DATA for SMB Header in Hex
+            if smb_header_negotiate in pkt_hex:
+                # Send response if Header is found.
+                event = "[*] SMB Header - Negotiate Session was detected from{0}".format(\
+                    self.client_address[0])
+                print event
+                write_event_log_event(event)
+                response_file = smb_negotiate_response
+                self.send_response(response_file)
+            if smb_header_session_setup in pkt_hex:
+                # Send account disabled response to start up request.
+                event = "[*] SMB Header - Session Setup detected from {0}".format(\
+                    self.client_address[0]) 
+                print event
+                write_event_log_event(event)
+                response_file = smb_session_startup_response
+                self.send_response(response_file)
+            if smb_header_account_disabled in pkt_hex:
+                # Send LANMAN info to requester
+                event = "[*] SMB Header - LANMAN information requested from {0}\
+                ".format(self.client_address[0])
+                print event
+                write_event_log_event(event)
+                response_file = smb_account_disabled_repsonse
+                self.send_response(response_file)
+            if smb_session_close in pkt_hex:
+                # Send session close.
+                event  = "[*] SMB Header - Session Close detected from {0}".format(\
+                    self.client_address[0])
+                print event
+                write_event_log_event(event)
+                response_file = smb_close_response
+                self.send_response(response_file)
+            else:
+                self.request.sendall(self.DATA)
+        except Exception as log_error:
+            log_error = "[!] Error receiving data from socket with {0} because {1}".format(\
+                self.client_address[0], error)
+            print log_error
+            write_error_log_event(str(log_error))
+
+
+    def send_response(self, response_file):
+        self.response_file = response_file
+        with open(self.response_file, 'rb') as f:
+            response = f.read()
+        self.request.sendall(response)
 
 
 class HoneyPotHandler(Thread):
@@ -80,11 +106,15 @@ class HoneyPotHandler(Thread):
             x = TCPEchoHandler
         try:
             server = SocketServer.TCPServer((LHOST, int(self.port)), x)
-            print "[*] {0} handler started on {1}:{2}\n".format(x, LHOST, self.port)
+            event = "[*] {0} handler started on {1}:{2}".format(x, LHOST, self.port)
+            print event
+            write_event_log_event(event)
             server.serve_forever()
-        except Exception as log_error:
-            print "[!] There was an error: {0} ".format(log_error)
-            write_error_log_event(log_error)
+        except Exception as error:
+            error = "[!] There was an error establishing a handler because {0}".format(\
+                                                                                error)
+            print error
+            write_error_log_event(error)
 
 
 def build_ports_list():
@@ -94,14 +124,17 @@ def build_ports_list():
         global ports
         ports = i.readlines()
         ports = [x.strip('\n') for x in ports]
-    print "[*] will start listening on: {0}".format(ports)
+    event = "[*] will start listening on: {0}".format(ports)
+    write_event_log_event(event)
     return ports
 
 
 def build_pot():
     thread_list = []
     for port in ports:
-        print "[*] Starting handler on port {0}".format(port)
+        event = "[*] Starting handler on port {0}".format(port)
+        print event
+        write_event_log_event(event)
         t = HoneyPotHandler(port)
         thread_list.append(t)
     for thread in thread_list:
@@ -114,14 +147,14 @@ def write_event_log_event(event):
     # add datetime to log name
     log_file = "event.log"
     with open(log_file, 'a') as l:
-        l.write(event)
+        l.write(event + "\n")
 
 
 def write_error_log_event(error):
     # add datetime to log name
     log_file = "error.log"
     with open(log_file, 'a') as l:
-        l.write(error)
+        l.write(error + "\n")
 
 
 def main():
