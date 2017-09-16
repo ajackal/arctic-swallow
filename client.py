eeb7ae6cc00ece4bb7cfeb792fb753f41dbb5341
cmill <ajackal244@gmail.com>
2017-09-15 22:55:38 -0400

    modified client.py for ctf

--- client.py
+++ client.py
@@ -1,18 +1,30 @@
 import socket
 import sys
+from itertools import product
 
+# builds key list
+characters_to_use = "0123456789"
+keys_to_try = product(characters_to_use, repeat=2)
+
+for key_pair in keys_to_try:
+    key = key_pair[0] + key_pair[1]
+    # print key
+
+# define ip address and port to use
 ip_addr = 'localhost'
-tcp_port = raw_input("[*] Connect to port: ")
+tcp_port = 80
 
 buffer_size = 1024
 message_to_server = "bite my shiny metal @ss"
 
+# opens socket
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 except Exception as e:
     print e
 
-s.connect((ip_addr, int(tcp_port)))
+# connects socket to ip address and port defined above
+s.connect((ip_addr, tcp_port))
 
 try:
     s.send(message_to_server)
